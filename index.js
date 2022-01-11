8bcb41ca9242663ffefe207abebdf0604f9d7e90
HiagoCS <hiago8cs@gmail.com>
2022-01-11 20:00:43 -0300

    update Discord JS modules to v13

---
+++ index.js
@@ -0,0 +1,15 @@
+require('dotenv').config();
+
+const Client = require("./src/structures/Client");
+
+const bot = new Client({
+	intents:[
+		'GUILDS',
+		'GUILD_MESSAGE_REACTIONS',
+		'GUILD_MESSAGES',
+		'GUILD INVITES',
+		'GUILD_VOICE_STATES',
+		'GUILD_MEMBERS',
+		'GUILD PRESENCES'
+	]
+})
