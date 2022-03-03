41fffaececb36517176dcc2f877cdedcf1d8024f
HiagoCS <hiago8cs@gmail.com>
2022-03-03 15:28:38 -0300

    minimal change in ready.js

--- src/events/client/ready.js
+++ src/events/client/ready.js
@@ -1,4 +1,5 @@
 const Event = require('../../structures/Event');
+const fs = require('fs');
 
 module.exports = class extends Event{
 	constructor(client){
@@ -9,5 +10,9 @@ module.exports = class extends Event{
 	run = () =>{
 		console.log(`${this.client.user.username} está pronto pra executar Plebs com Prime :)`)
 		this.client.registryCommands();
+		const filesList = fs.readdirSync("./JSON");
+		if(!filesList.includes('fichas')){
+			fs.mkdirSync("./JSON/fichas");
+		}
 	}
 }
